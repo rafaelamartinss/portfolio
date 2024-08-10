90f294798a89f2542d58706d17fb28ca59ff7229
Rafaela Leal <rafaelalealmartinss@gmail.com>
2024-08-10 16:08:58 -0300

    Let's go!

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+        fontFamily: {
+          archivo: ['Archivo Black', 'sans-serif'],
+        },
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
